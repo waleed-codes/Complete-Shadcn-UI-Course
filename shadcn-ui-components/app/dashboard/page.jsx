fb2d06638f220876becb4d350d1f76f970bbfb01
Waleed Ishfaq <110061477+waleedcodes@users.noreply.github.com>
2024-12-14 21:10:33 +0500

    final complete code of shadcn ui course

---
+++ shadcn-ui-components/app/dashboard/page.jsx
@@ -0,0 +1,53 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import CardComponents from "../_components/CardComponents";
+import { AreachatComp } from "../_components/AreachatComp";
+import { BarChatComp } from "../_components/BarChatComp";
+import { RadialChartComp } from "../_components/RadialChartComp";
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="grid grid-cols-3 gap-5">
+          <AreachatComp />
+          <BarChatComp />
+          <RadialChartComp />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
